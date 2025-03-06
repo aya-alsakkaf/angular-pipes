ba5054909cc325dd854a1b0e65b773c325c07728
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-06 13:16:30 +0300

    accepting arguments for pipes

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { input, Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'temp',
@@ -7,16 +7,32 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class TemperaturePipe implements PipeTransform {
     //value : the value on which the pipe is used
     // args : the configuration value for the pipe
-    transform(value: string | number, ...args: any[]) {
+    transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
         let val: number;
         if (typeof value === 'string') {
             val = parseFloat(value)
         } else {
             val = value
         }
+        let outputTemp: number;
 
-        const outputTemp = val * (9 / 5) + 32
-        return `${outputTemp.toFixed(1)} °F`;
+        if (inputType === 'cel' && outputType === 'fah') {
+            outputTemp = val * (9 / 5) + 32
+        } else if (inputType === 'fah' && outputType === 'cel') {
+            outputTemp = (val - 32) & (5 / 9)
+        } else {
+            outputTemp = val;
+        }
+
+        let symbol: '°C' | '°F'
+
+        if (!outputType) {
+            symbol = inputType === 'cel' ? "°C" : "°F"
+        } else {
+            symbol = outputType === 'cel' ? "°C" : "°F"
+
+        }
+        return `${outputTemp.toFixed(1)} ${symbol}`;
     }
 
 }
